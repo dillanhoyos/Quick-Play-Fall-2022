4f3b6b9434320a14a96b9f8f93cd288e0ddd8ec5
Isaro <55099931+dillanhoyos@users.noreply.github.com>
2022-10-11 21:19:21 -0400

    This is basic controls for Mobile
    added UI for Joystick and controls.
    Used Cinemachine for Camera.

---
+++ My project/Assets/ThirdPersonMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Joystick joystick;
+    public Transform cam;
+
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    public float turnSmoothVelocity;
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = joystick.Horizontal;
+        float vertical = joystick.Vertical;
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+         
+
+        if(direction.magnitude >=0.1f)
+
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+    }
+}
