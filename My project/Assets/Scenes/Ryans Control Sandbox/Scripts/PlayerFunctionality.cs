9bc876633ed0b06bcee5a1f45dd8f1e82f3cd759
Ryan Beckett <pook0301001@gmail.com>
2022-10-15 14:23:07 -0400

    Control -- Update

---
+++ My project/Assets/Scenes/Ryans Control Sandbox/Scripts/PlayerFunctionality.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFunctionality : MonoBehaviour
+{
+    public bool isAttacking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isAttacking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Attack()
+    {
+        Debug.Log("Attack button PRESSED");
+    }
+}
--- My project/Assets/ThirdPersonMovement.cs
+++ My project/Assets/ThirdPersonMovement.cs
@@ -6,7 +6,6 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Joystick joystick;
-    public Touch button;
     public Transform cam;
 
     public float speed = 10f;
@@ -19,8 +18,6 @@ public class ThirdPersonMovement : MonoBehaviour
         float horizontal = joystick.Horizontal;
         float vertical = joystick.Vertical;
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        gravity += (Physics.gravity.y * Time.deltaTime);
          
         if(direction.magnitude >=0.1f)
 
@@ -31,6 +28,5 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-
     }
 }
