22a899ec92fc75bda5c5621c117c89a6018bc65a
Isaro <55099931+dillanhoyos@users.noreply.github.com>
2022-10-17 23:58:40 -0400

    New scene
    here

--- My project/Assets/ThirdPersonMovement.cs
+++ My project/Assets/ThirdPersonMovement.cs
@@ -7,27 +7,49 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Joystick joystick;
     public Transform cam;
-
-    public Rigidbody PlayerBody;
-
+ 
     public float speed = 10f;
     public float turnSmoothTime = 0.1f;
     public float turnSmoothVelocity;
-    public float JumpSpeed;
 
-    public bool isJumping;
+
+    private Vector3 _playerVelocity;
+
+    private bool _groundedPlayer;
+    [SerializeField] private float _jumpHeight = 5.0f;
+
+    private bool _jumpPressed = false;
+    private float _gravityValue = -9.81f;
+
+
+    ///JUMP
+    public float jumpspeed = 5;
+    public bool isGrounded;
+
 
     void Start()
     {
-        PlayerBody = FindObjectOfType<Rigidbody>();
-        isJumping = false;
+        controller = GetComponent<CharacterController>();
+     
+   
 
-        JumpSpeed = 100f;
+       
     }
-
 
     // Update is called once per frame
     void Update()
+    {   
+   
+
+        Joystick();
+        MovementJump();
+
+    }
+
+
+
+
+    public void Joystick()
     {
         float horizontal = joystick.Horizontal;
         float vertical = joystick.Vertical;
@@ -42,15 +64,59 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
     }
 
     public void PlayerJump()
     {
-        if (!isJumping)
+
+        //Check if no vertical movement
+        if(controller.velocity.y == 0)
         {
-            PlayerBody.velocity += Vector3.up * JumpSpeed;
-            Debug.Log("JUMP PRESSED");
+            Debug.Log("CanJump");
+            _jumpPressed = true;
+
+
+        }
+        else
+        {
+            Debug.Log("noJump");
+        }
+           
+    }
+
+    void MovementJump()
+    {
+        _groundedPlayer = controller.isGrounded;
+
+        if (_groundedPlayer)
+        {
+            _playerVelocity.y = 0.0f;
+        }
+        if (_jumpPressed && _groundedPlayer)
+        {
+            _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -1* _gravityValue);
+            _jumpPressed = false;
+
+        }
+
+        _playerVelocity.y += _gravityValue * Time.deltaTime;
+        controller.Move(_playerVelocity * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Ground"))
+        {
+            isGrounded = true;
         }
     }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
 }
 
